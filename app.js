cf653776b480b0b28c0838d07e9e6673408a9a13
unknown <luispomare92@gmail.com>
2022-02-13 20:51:21 -0500

    Create dynamic elements with JS

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //  Main menu interactions
-
 //  Extract hamburguer menu icon object from DOM
 const menuIcon = document.getElementById("menuIcon");
 
@@ -20,17 +19,17 @@ const thirdMenuElement = document.createElement("a");
 const fourthMenuElement = document.createElement("a");
 const fifthMenuElement = document.createElement("a");
 
-firstMenuElement.target = "-blank";
-firstMenuElement.href = "aboutPage.html";
-secondMenuElement.href = "index.html";
-thirdMenuElement.href = "#footer";
-fourthMenuElement.href = "#footer";
-fifthMenuElement.href = "#footer";
 firstMenuElement.innerText = "About";
 secondMenuElement.innerText = "Program";
 thirdMenuElement.innerText = "Join";
 fourthMenuElement.innerText = "Sponsors";
 fifthMenuElement.innerText = "News";
+firstMenuElement.target = "-blank";
+firstMenuElement.href = "aboutPage.html";
+secondMenuElement.href = "index.html";
+thirdMenuElement.href = "#footerIcon";
+fourthMenuElement.href = "#footerIcon";
+fifthMenuElement.href = "#footerIcon";
 
 //  Apend menu elements to nav container
 dynamicNav.appendChild(closerIcon);
@@ -49,8 +48,130 @@ function deactiveMenu() {
   dynamicNav.remove();
 }
 
+// Listeners to nav elements interactions
 menuIcon.addEventListener("click", activeMenu);
 closerIcon.addEventListener("click", deactiveMenu);
 firstMenuElement.addEventListener("click", deactiveMenu);
 secondMenuElement.addEventListener("click", deactiveMenu);
 thirdMenuElement.addEventListener("click", deactiveMenu);
+fourthMenuElement.addEventListener("click", deactiveMenu);
+fifthMenuElement.addEventListener("click", deactiveMenu);
+
+// Featured speakers section
+
+//  Objects declaration
+const firstSpeaker = {
+  Photo: "images/featuredImageOne.png",
+  Name: "First Speaker",
+  Background: "First background",
+  Review: "First lorem ipsum common dole",
+};
+const secondSpeaker = {
+  Photo: "images/featuredImageTwo.png",
+  Name: "Second Speaker",
+  Background: "Second background",
+  Review: "Second lorem ipsum common dole",
+};
+const thirdSpeaker = {
+  Photo: "images/featuredImageThree.png",
+  Name: "Third Speaker",
+  Background: "Third background",
+  Review: "Third lorem ipsum common dole",
+};
+const fourthSpeaker = {
+  Photo: "images/featuredImageFour.png",
+  Name: "Fourth Speaker",
+  Background: "Fourth background",
+  Review: "Fourth lorem ipsum common dole",
+};
+const fifthSpeaker = {
+  Photo: "images/featuredImageFive.png",
+  Name: "Fifth Speaker",
+  Background: "Fifth background",
+  Review: "Fifth lorem ipsum common dole",
+};
+const sixthtSpeaker = {
+  Photo: "images/featuredImageSix.png",
+  Name: "Sixth Speaker",
+  Background: "Sixth background",
+  Review: "Sixth lorem ipsum common dole",
+};
+
+const speakerArray = [
+  firstSpeaker,
+  secondSpeaker,
+  thirdSpeaker,
+  fourthSpeaker,
+  fifthSpeaker,
+  sixthtSpeaker,
+];
+
+// Get elemnts from HTML file
+//  images
+const featuredImageOne = document.getElementById("featuredImageOne");
+const featuredImageTwo = document.getElementById("featuredImageTwo");
+const featuredImageThree = document.getElementById("featuredImageThree");
+const featuredImageFour = document.getElementById("featuredImageFour");
+const featuredImageFive = document.getElementById("featuredImageFive");
+const featuredImageSix = document.getElementById("featuredImageSix");
+
+//  Speakers names
+const featuredTittleOne = document.getElementById("featuredTittleOne");
+const featuredTittleTwo = document.getElementById("featuredTittleTwo");
+const featuredTittleThree = document.getElementById("featuredTittleThree");
+const featuredTittleFour = document.getElementById("featuredTittleFour");
+const featuredTittleFive = document.getElementById("featuredTittleFive");
+const featuredTittleSix = document.getElementById("featuredTittleSix");
+
+//  Speakers background
+const featuredSubtittleOne = document.getElementById("featuredSubtittleOne");
+const featuredSubtittleTwo = document.getElementById("featuredSubtittleTwo");
+const featuredSubtittleThree = document.getElementById(
+  "featuredSubtittleThree"
+);
+const featuredSubtittleFour = document.getElementById("featuredSubtittleFour");
+const featuredSubtittleFive = document.getElementById("featuredSubtittleFive");
+const featuredSubtittleSix = document.getElementById("featuredSubtittleSix");
+
+//  Speakers review
+const featuredParagraphOne = document.getElementById("featuredParagraphOne");
+const featuredParagraphTwo = document.getElementById("featuredParagraphTwo");
+const featuredParagraphThree = document.getElementById(
+  "featuredParagraphThree"
+);
+const featuredParagraphFour = document.getElementById("featuredParagraphFour");
+const featuredParagraphFive = document.getElementById("featuredParagraphFive");
+const featuredParagraphSix = document.getElementById("featuredParagraphSix");
+
+//  Give the information to the page
+// Images
+featuredImageOne.src = speakerArray[0].Photo;
+featuredImageTwo.src = speakerArray[1].Photo;
+featuredImageThree.src = speakerArray[2].Photo;
+featuredImageFour.src = speakerArray[3].Photo;
+featuredImageFive.src = speakerArray[4].Photo;
+featuredImageSix.src = speakerArray[5].Photo;
+
+//  Speaker Names
+featuredTittleOne.innerText = speakerArray[0].Name;
+featuredTittleTwo.innerText = speakerArray[1].Name;
+featuredTittleThree.innerText = speakerArray[2].Name;
+featuredTittleFour.innerText = speakerArray[3].Name;
+featuredTittleFive.innerText = speakerArray[4].Name;
+featuredTittleSix.innerText = speakerArray[5].Name;
+
+//  Speaker Background
+featuredSubtittleOne.innerText = speakerArray[0].Background;
+featuredSubtittleTwo.innerText = speakerArray[1].Background;
+featuredSubtittleThree.innerText = speakerArray[2].Background;
+featuredSubtittleFour.innerText = speakerArray[3].Background;
+featuredSubtittleFive.innerText = speakerArray[4].Background;
+featuredSubtittleSix.innerText = speakerArray[5].Background;
+
+//  Speaker Review
+featuredParagraphOne.innerText = speakerArray[0].Review;
+featuredParagraphTwo.innerText = speakerArray[1].Review;
+featuredParagraphThree.innerText = speakerArray[2].Review;
+featuredParagraphFour.innerText = speakerArray[3].Review;
+featuredParagraphFive.innerText = speakerArray[4].Review;
+featuredParagraphSix.innerText = speakerArray[5].Review;
